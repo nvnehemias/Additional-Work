d3e244017a1a80db2381ff89f5440ef8c58de1c3
Nehemias Vicente <nvnehemias@gmail.com>
2019-08-22 14:25:49 -0700

    Adding calculator file

---
+++ Python/calculator.py
@@ -0,0 +1,25 @@
+"""
+Calculator library containing basic math operations
+
+"""
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+def subtract(first_number, second_number):
+    return first_number - second_number
+
+def multiply(first_number, second_number):
+    return first_number * second_number
+
+def divide(first_number, second_number):
+    if not first_number = 0:
+        return second_number/first_number
+    else:
+        print(f"Your first number {first_number} cannot be zero")
+
+def square(number):
+    return number*number
+
+
+        
