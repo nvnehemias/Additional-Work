318b25c9b383c06671f02c6f18c3850b7e73b85b
Nehemias Vicente <nvnehemias@gmail.com>
2019-09-06 10:34:17 -0700

    New files of Javascript folder

---
+++ Javascript/this_function/app.js
@@ -0,0 +1,13 @@
+d3.selectAll("button").on("click", function() {
+  // What will be logged out? What is `this` in this case?
+  console.log(this);
+});
+
+d3.selectAll("li").on("click",function(){
+// you can select the element just like any other selection
+  var listItem = d3.select(this);
+  listItem.style("color","blue");
+
+  var listItemText = listItem.text()
+  console.log(listItemText);
+})
