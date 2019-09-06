645036e05f09a72f5c4ea53decbcae851a319aaf
Nehemias Vicente <nvnehemias@gmail.com>
2019-09-06 12:02:24 -0700

    Adding d3 table file

---
+++ Javascript/d3_table/static/js/app.js
@@ -0,0 +1,23 @@
+// The new student and grade to add to the table
+var newGrade = ["Wash", 79];
+
+// Use D3 to select the table
+var select_table = d3.select("table");
+// Use d3 to create a bootstrap striped table
+// http://getbootstrap.com/docs/3.3/css/#tables-striped
+
+select_table.attr("class","table table-striped");
+
+// Use D3 to select the table body
+
+var body = d3.select("tbody");
+
+// Append one table row `tr` to the table body
+
+var row = body.append("tr");
+
+// Append one cell for the student name
+row.append("td").text(newGrade[0]);
+
+// Append one cell for the student grade
+row.append("td").text(newGrade[1]);
